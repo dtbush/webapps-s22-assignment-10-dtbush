7647697f134c53bdb863e4cf512e05b7bc275617
Drake Bush <54418983+dtbush@users.noreply.github.com>
2022-04-07 23:27:45 -0500

    part 6 complete

--- routes/denim.js
+++ routes/denim.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('denim', { title: 'Express' });
+  res.render('denim', { title: 'Denim' });
 });
 
 module.exports = router;
