d9f1bcf4b3495ac3a02289022e0ab9e8975eeaec
Drake Bush <54418983+dtbush@users.noreply.github.com>
2022-04-07 21:12:33 -0500

    /denim endpoint

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var denimRouter = require('./routes/denim');
 var app = express();
 
 // view engine setup
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/denim', denimRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
